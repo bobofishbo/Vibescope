73aa9b03def11c76765d39224a2c883ed68f8e10
bobofishbo <144623157+bobofishbo@users.noreply.github.com>
2024-12-19 14:05:36 -0500

    initial commit

---
+++ vibescope/src/_auth/AuthLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const AuthLayout = () => {
+    return (
+        <div>Sigma boy</div>
+    )
+
+}
+
+export default AuthLayout
---
+++ vibescope/src/_root/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <div>Sigma boy</div>
+    )
+
+}
+
+export default Home
